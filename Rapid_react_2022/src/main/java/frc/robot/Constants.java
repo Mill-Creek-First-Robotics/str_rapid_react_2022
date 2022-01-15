abf6d0dbc5ec2fbca6e34bdac55c3caa3834f7e1
SteelTalonsTerminal <steeltalons6910@gmail.com>
2022-01-15 11:37:24 -0500

    made drive train for mecanum drive and filled in placeholders for constants

--- Rapid_react_2022/src/main/java/frc/robot/Constants.java
+++ Rapid_react_2022/src/main/java/frc/robot/Constants.java
@@ -12,4 +12,13 @@ package frc.robot;
  * <p>It is advised to statically import this class (or one of its inner classes) wherever the
  * constants are needed, to reduce verbosity.
  */
-public final class Constants {}
+public final class Constants 
+{
+    public static final int FRONT_LEFT_MOTOR = 1;
+    public static final int BACK_LEFT_MOTOR = 2;
+    public static final int FRONT_RIGHT_MOTOR = 3;
+    public static final int BACK_RIGHT_MOTOR = 4;
+
+    public static final int ENCODER = 5;
+    public static final int GYROSCOPE = 6;
+}
---
+++ Rapid_react_2022/src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -0,0 +1,48 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import edu.wpi.first.wpilibj.drive.MecanumDrive;
+import frc.robot.Constants;
+
+public class Drivetrain extends SubsystemBase {
+
+  WPI_TalonSRX frontLeftMotor = null;
+  WPI_TalonSRX backLeftMotor = null;
+  WPI_TalonSRX frontRightMotor = null;
+  WPI_TalonSRX backRightMotor = null;
+  public MecanumDrive mecanumDrive;
+
+  public Drivetrain() 
+  {
+    frontLeftMotor = new WPI_TalonSRX(Constants.FRONT_LEFT_MOTOR);
+    backLeftMotor = new WPI_TalonSRX(Constants.BACK_LEFT_MOTOR);
+    frontRightMotor = new WPI_TalonSRX(Constants.FRONT_RIGHT_MOTOR);
+    backRightMotor = new WPI_TalonSRX(Constants.BACK_RIGHT_MOTOR);
+    
+    mecanumDrive = new MecanumDrive(frontLeftMotor, backLeftMotor, frontRightMotor, backRightMotor);
+  }
+
+  //Angles are measured clockwise from the positive X axis. The robot's speed is independent from its angle or rotation rate.
+  //Gyro is feild oreintation while zRotation is relative to the robot
+  public void driveCartesian(double ySpeed, double xSpeed, double zRotation, double gyroAngle)
+  {
+    mecanumDrive.driveCartesian(ySpeed, xSpeed, zRotation, gyroAngle);
+  }
+
+  @Override
+  public void periodic() 
+  {
+    // This method will be called once per scheduler run
+  }
+
+  @Override
+  public void simulationPeriodic() 
+  {
+    // This method will be called once per scheduler run during simulation
+  }
+}
