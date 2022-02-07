f9a9ec3900b63ec54602b96c13fbfcede8943d9a
YeahBTW <austin.j.mann@gmail.com>
2022-02-07 14:59:25 -0500

    updated better the drive system

--- Rapid_react_2022/src/main/java/frc/robot/subsystems/drivetrain/Drivetrain.java
+++ Rapid_react_2022/src/main/java/frc/robot/subsystems/drivetrain/Drivetrain.java
@@ -88,8 +88,7 @@ public class Drivetrain extends SubsystemBase {
 
   public static void supremeTankDrivePart2BattleOfTheWheels(double forwardSpeed, double speedLimit,  double rotationX, double rotationY)
   {
-    double calculatedGyroAngle = (gyroscope.getAngle() % 360);
-    if (calculatedGyroAngle > 180){calculatedGyroAngle -= 360;}
+    double calculatedGyroAngle = gyroscope.getYaw();
 
     targetAngle = Math.toDegrees(Math.atan2(rotationY, rotationX) + Math.PI) - calculatedGyroAngle;
 
