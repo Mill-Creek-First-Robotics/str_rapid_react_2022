60d2412173619f47b14368c931b769a145a52275
SteelTalonsTerminal <steeltalons6910@gmail.com>
2022-02-26 09:39:21 -0500

    got drivetrain workin

--- Rapid_react_2022-Imported/src/main/java/frc/robot/Robot.java
+++ Rapid_react_2022-Imported/src/main/java/frc/robot/Robot.java
@@ -5,6 +5,8 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.XboxController;
+import frc.robot.subsystems.drivetrain.Drivetrain;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 /*
@@ -17,7 +19,8 @@ public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
 
   private RobotContainer m_robotContainer;
-
+  private Drivetrain m_drivetrain = new Drivetrain();
+  private XboxController stick = new XboxController(Constants.CONTROLLER);
   /**
    * This function is run when the robot is first started up and should be used for any
    * initialization code.
@@ -27,6 +30,8 @@ public class Robot extends TimedRobot {
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
     m_robotContainer = new RobotContainer();
+
+
   }
 
   /**
@@ -56,7 +61,7 @@ public class Robot extends TimedRobot {
   @Override
   public void autonomousInit() {
     m_autonomousCommand = m_robotContainer.getAutonomousCommand();
-
+    
     // schedule the autonomous command (example)
     if (m_autonomousCommand != null) {
       m_autonomousCommand.schedule();
@@ -73,6 +78,7 @@ public class Robot extends TimedRobot {
     // teleop starts running. If you want the autonomous to
     // continue until interrupted by another command, remove
     // this line or comment it out.
+
     if (m_autonomousCommand != null) {
       m_autonomousCommand.cancel();
     }
@@ -80,7 +86,9 @@ public class Robot extends TimedRobot {
 
   /** This function is called periodically during operator control. */
   @Override
-  public void teleopPeriodic() {}
+  public void teleopPeriodic() {
+    m_drivetrain.classicDrive(-stick.getLeftY() * .5, -stick.getRightY() * .5);
+  }
 
   @Override
   public void testInit() {
@@ -90,9 +98,10 @@ public class Robot extends TimedRobot {
 
   /** This function is called periodically during test mode. */
   @Override
-  public void testPeriodic() {
-
-
-
+  public void testPeriodic() 
+  {
+    
   }
+
+
 }
